b43ce34d85aa05c32849b2130ea1075ac08db2c8
HenryLee <henrylee_cn@foxmail.com>
2016-08-01 19:23:53 +0800

    update

---
+++ example/example.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"strconv"
+	"time"
+
+	"github.com/henrylee2cn/rpc2"
+)
+
+type Worker struct {
+	Name string
+}
+
+func NewWorker() *Worker {
+	return &Worker{"test"}
+}
+
+func (w *Worker) DoJob(task string, reply *string) error {
+	// log.Println("Worker: do job", task)
+	// time.Sleep(time.Second * 3)
+	*reply = "OK"
+	return nil
+}
+
+// rpc2
+func main() {
+	// server
+	rpc2.Register(NewWorker())
+	rpc2.AllowClients("127.0.0.1")
+	go rpc2.ListenRPC("0.0.0.0:80")
+	time.Sleep(2e9)
+
+	// client
+	N := 1000
+	bad := 0
+	good := 0
+	mapChan := make(chan int, N)
+	t1 := time.Now()
+	for ii := 0; ii < 10; ii++ {
+		for i := 0; i < N; i++ {
+			go func(i int) {
+				var reply = new(string)
+				e := rpc2.Call("127.0.0.1:80", "Worker.DoJob", strconv.Itoa(ii*N+i), reply)
+				log.Println(ii*N+i, *reply, e)
+				if e != nil {
+					mapChan <- 0
+				} else {
+					mapChan <- 1
+				}
+			}(i)
+		}
+		for i := 0; i < N; i++ {
+			if r := <-mapChan; r == 0 {
+				bad++
+			} else {
+				good++
+			}
+		}
+	}
+	log.Println("cost time:", time.Now().Sub(t1))
+	log.Println("failure rate:", float64(bad)/float64(good)*100, "%")
+}
+
+// standard package
+func main2() {
+	// server
+	rpc.Register(NewWorker())
+	rpc.HandleHTTP()
+
+	l, e := net.Listen("tcp", "127.0.0.1:80") // any available address
+	if e != nil {
+		log.Fatalf("net.Listen tcp :0: %v", e)
+	}
+
+	go http.Serve(l, nil)
+	time.Sleep(2e9)
+
+	// client
+	N := 1000
+	bad := 0
+	good := 0
+	mapChan := make(chan int, N)
+	t1 := time.Now()
+	for ii := 0; ii < 10; ii++ {
+		for i := 0; i < N; i++ {
+			go func(i int) {
+				var reply = new(string)
+				var err error
+				defer func() {
+					if err != nil {
+						mapChan <- 0
+					} else {
+						mapChan <- 1
+					}
+					log.Println(ii*N+i, *reply, err)
+				}()
+
+				client, err := rpc.DialHTTP("tcp", "127.0.0.1:80")
+				if err != nil {
+					log.Println("dialing:", err)
+					return
+				}
+
+				err = client.Call("Worker.DoJob", strconv.Itoa(ii*N+i), reply)
+				if err != nil {
+					log.Println("arith error:", err)
+					return
+				}
+
+			}(i)
+		}
+		for i := 0; i < N; i++ {
+			if r := <-mapChan; r == 0 {
+				bad++
+			} else {
+				good++
+			}
+		}
+	}
+	log.Println("cost time:", time.Now().Sub(t1))
+	log.Println("failure rate:", float64(bad)/float64(good)*100, "%")
+
+}
---
+++ server.go
@@ -0,0 +1,154 @@
+package rpc2
+
+import (
+	"bufio"
+	"encoding/gob"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/rpc"
+	"strings"
+	"sync"
+	"time"
+)
+
+type ClientIPs struct {
+	ips       map[string]bool
+	ipPrefixs []string
+	sync.RWMutex
+}
+
+var clientIPs = &ClientIPs{
+	ips: map[string]bool{},
+
+	// lan is allowed
+	ipPrefixs: []string{
+		"[",
+		"127.",
+		"192.168.",
+		"",
+		"10.",
+	},
+}
+
+func (this *ClientIPs) notAllow(addr string) bool {
+	this.RLock()
+	defer this.RUnlock()
+	ip := strings.Split(addr, ":")[0]
+	if this.ips[ip] {
+		return false
+	}
+	for i, count := 0, len(this.ipPrefixs); i < count; i++ {
+		if strings.HasPrefix(ip, this.ipPrefixs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func AllowClients(ips ...string) {
+	clientIPs.Lock()
+	defer clientIPs.Unlock()
+	for _, ip := range ips {
+		clientIPs.ips[ip] = true
+	}
+}
+
+func TimeoutCoder(f func(interface{}) error, e interface{}, msg string) error {
+	echan := make(chan error, 1)
+	go func() { echan <- f(e) }()
+	select {
+	case e := <-echan:
+		return e
+	case <-time.After(time.Minute):
+		return fmt.Errorf("Timeout %s", msg)
+	}
+}
+
+type gobServerCodec struct {
+	rwc    io.ReadWriteCloser
+	dec    *gob.Decoder
+	enc    *gob.Encoder
+	encBuf *bufio.Writer
+	closed bool
+}
+
+func (c *gobServerCodec) ReadRequestHeader(r *rpc.Request) error {
+	return TimeoutCoder(c.dec.Decode, r, "server read request header")
+}
+
+func (c *gobServerCodec) ReadRequestBody(body interface{}) error {
+	return TimeoutCoder(c.dec.Decode, body, "server read request body")
+}
+
+func (c *gobServerCodec) WriteResponse(r *rpc.Response, body interface{}) (err error) {
+	if err = TimeoutCoder(c.enc.Encode, r, "server write response"); err != nil {
+		if c.encBuf.Flush() == nil {
+			log.Println("rpc: gob error encoding response:", err)
+			c.Close()
+		}
+		return
+	}
+	if err = TimeoutCoder(c.enc.Encode, body, "server write response body"); err != nil {
+		if c.encBuf.Flush() == nil {
+			log.Println("rpc: gob error encoding body:", err)
+			c.Close()
+		}
+		return
+	}
+	return c.encBuf.Flush()
+}
+
+func (c *gobServerCodec) Close() error {
+	if c.closed {
+		// Only call c.rwc.Close once; otherwise the semantics are undefined.
+		return nil
+	}
+	c.closed = true
+	return c.rwc.Close()
+}
+
+// be called before 'ListenRPC'
+func Register(rcvrs ...interface{}) {
+	for _, rcvr := range rcvrs {
+		rpc.Register(rcvr)
+	}
+}
+
+func ListenRPC(addr string) {
+	l, e := net.Listen("tcp", addr)
+	if e != nil {
+		log.Fatal("Error: listen %s error:", addr, e)
+	}
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Println("Error: accept rpc connection", err.Error())
+				continue
+			}
+
+			// filter ip
+			if clientIPs.notAllow(conn.RemoteAddr().String()) {
+				log.Println("Client Not allow:", conn.RemoteAddr().String())
+				continue
+			}
+
+			go func(conn net.Conn) {
+				buf := bufio.NewWriter(conn)
+				srv := &gobServerCodec{
+					rwc:    conn,
+					dec:    gob.NewDecoder(conn),
+					enc:    gob.NewEncoder(buf),
+					encBuf: buf,
+				}
+				err = rpc.ServeRequest(srv)
+				if err != nil {
+					log.Print("Error: server rpc request", err.Error())
+				}
+				srv.Close()
+			}(conn)
+		}
+	}()
+}
