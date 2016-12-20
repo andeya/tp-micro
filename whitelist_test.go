4dd9b5b6b742282a1febdf92b70a475ad14b0d7a
henrylee2cn <henrylee_cn@foxmail.com>
2016-12-20 11:46:10 +0800

    Add test

---
+++ whitelist_test.go
@@ -0,0 +1,25 @@
+package rpc2
+
+import (
+	"testing"
+)
+
+func TestEmptyIPWhitelist(t *testing.T) {
+	w := NewIPWhitelist()
+	w.Allow([]string{}...)
+	if !w.IsAllowed("202.0.123.12") {
+		t.Log("not allowed")
+	} else {
+		t.Log("allowed")
+	}
+}
+
+func TestIPWhitelist(t *testing.T) {
+	w := NewIPWhitelist()
+	w.Allow("9*")
+	if !w.IsAllowed("202.0.123.12") {
+		t.Log("not allowed")
+	} else {
+		t.Log("allowed")
+	}
+}
