1a2f0dfd745ff3aef66492abc040921f2c22983b
henrylee2cn <henrylee_cn@foxmail.com>
2018-03-21 11:13:05 +0800

    command: ‘ant gen’ update test

--- cmd/ant/create/generator_test.go
+++ cmd/ant/create/generator_test.go
@@ -4,9 +4,11 @@ import (
 	"testing"
 
 	"github.com/henrylee2cn/ant/cmd/ant/create/test"
+	"github.com/henrylee2cn/ant/cmd/ant/info"
 )
 
 func TestGenerator(t *testing.T) {
+	info.Init("test")
 	src := test.MustAsset(defAntTpl)
 	proj := NewProject(src)
 	proj.Prepare()
@@ -14,6 +16,7 @@ func TestGenerator(t *testing.T) {
 	proj.genRouterFile()
 	proj.genHandlerAndLogicAndSdkFiles()
 	t.Logf("types/types.gen.go:\n%s", codeFiles["types/types.gen.go"])
+	t.Logf("logic/tmp_code.gen.go:\n%s", codeFiles["logic/tmp_code.gen.go"])
 	t.Logf("api/handler.gen.go:\n%s", codeFiles["api/handler.gen.go"])
 	t.Logf("api/router.gen.go:\n%s", codeFiles["api/router.gen.go"])
 	t.Logf("sdk/rpc.gen.go:\n%s", codeFiles["sdk/rpc.gen.go"])
